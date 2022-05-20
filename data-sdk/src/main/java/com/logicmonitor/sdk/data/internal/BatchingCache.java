62afd1637e61f022da93e2ac742500793c629eb2
Avleen <avleen.kathuria@logicmonitor.com>
2022-05-20 09:56:29 +0530

    Resolved-Build-fail

--- data-sdk/src/main/java/com/logicmonitor/sdk/data/internal/BatchingCache.java
+++ data-sdk/src/main/java/com/logicmonitor/sdk/data/internal/BatchingCache.java
@@ -244,7 +244,7 @@ public abstract class BatchingCache {
       } catch (Exception e) {
         log.error(e.getMessage());
       } finally {
-        if(gzip!=null && out!=null) {
+        if (gzip != null && out != null) {
           gzip.flush();
           gzip.close();
           out.close();
